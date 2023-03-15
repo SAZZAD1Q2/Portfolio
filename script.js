d2286ebcc88ea183bc471868067a0483ca7eb94b
SAZZAD1Q2 <kurulia115@gmail.com>
2023-03-15 21:30:30 +0600

    html and css file updated

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector('#btn_hambrg');
+
+
+btn.addEventListener('click',function() {
+    const navbar = document.querySelector('.navbar')
+    
+ 
+  const p = document.createElement('p');
+  p.textContent = 'dsfjadlkjfkalsjfkafjf';
+  p.style.display = 'block';
+  navbar.appendChild(p);
+   
+
+});
