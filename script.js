937b65167e756ff4e3214204b547f471d62163c9
SAZZAD1Q2 <kurulia115@gmail.com>
2023-03-16 06:39:59 +0600

    Js and readme file update

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const btn = document.querySelector('#btn_hambrg');
 
 btn.addEventListener('click', () => {
   const navbar = document.querySelector('.navbar');
-
-  navbar.style.disply = 'block';
   navbar.classList.toggle('show');
 });
 
