a8474fbecd170ac4819f7514aedbcfe11e98ba83
SAZZAD1Q2 <kurulia115@gmail.com>
2023-03-17 22:38:20 +0600

    linters file update

--- script.js
+++ script.js
@@ -367,3 +367,32 @@ workBtn6.addEventListener('click', () => {
     /* popup section see project button */
   });
 });
+
+const nameInput = document.querySelector('.inp-name');
+const emailInput = document.querySelector('.inp-email');
+const messageInput = document.querySelector('.inp-text');
+
+function saveFormData() {
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+function FormDataStore() {
+  const savedFormData = localStorage.getItem('formData');
+  if (savedFormData) {
+    const formData = JSON.parse(savedFormData);
+    nameInput.value = formData.name;
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+nameInput.addEventListener('input', saveFormData);
+emailInput.addEventListener('input', saveFormData);
+messageInput.addEventListener('input', saveFormData);
+
+FormDataStore();
